29b2cbb7d28fb479834928407a3c910a759258ad
7ommyShelby <aryaalsaher@gmail.com>
2024-12-28 20:37:42 +0530

    first

---
+++ data.js
@@ -0,0 +1,82 @@
+let arr = [
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 4,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 3,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 5,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 2,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 1,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 2,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 3,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 4,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 5,
+        votes : '400',
+        img: './sample.avif'
+    },
+    {
+        heading  : "This is a heading for a card",
+        para : "Lorem ipsum dolor sit amet consectetur adipisicing elit Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem assumenda repellendus nulla ipsam id officia",
+        level : "Beginner",
+        rating : 2,
+        votes : '400',
+        img: './sample.avif'
+    },
+]
---
+++ script.js
@@ -0,0 +1,86 @@
+const course = document.querySelector('.courses');
+
+console.log(arr);
+
+{/* <div class=" flex gap-2 max-w-full mt-4 bg-white rounded-lg p-2">
+
+    <div class="w-[30%] rounded-lg overflow-hidden">
+        <img class="h-full w-full" src="./sample.avif" alt="">
+    </div>
+
+    <div class="w-[70%]">
+        <h2 class="font-semibold">Learning strategy : <span class="italic font-semibold">how</span>
+            instead of what</h2>
+        <p class="text-slate-500 text-sm mt-1">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Consequatur doloremque maxime nam tempora reprehenderit ab omnis atque sint dolorem,
+            assumenda, repellendus nulla ipsam id officia?
+        </p>
+    </div>
+</div> */}
+
+function render() {
+
+    arr.forEach((card) => {
+
+        const cardDiv = document.createElement("div");
+        cardDiv.className = "flex gap-2 max-w-full mt-4 bg-white rounded-lg p-2";
+
+        const imgDiv = document.createElement('div')  //imgDivCon Parent
+        imgDiv.className = 'w-[30%] rounded-lg overflow-hidden'
+
+        const imgCon = document.createElement('img') //imgRaw child
+        imgCon.className = 'h-full w-full'
+        imgCon.setAttribute('src', card.img);
+
+
+        const contentDiv = document.createElement("div");
+        contentDiv.className = "w-[70%]";
+
+        const heading = document.createElement("h2");
+        heading.innerHTML = `${card.heading}`;
+        heading.className = "font-semibold text-lg";
+
+        const para = document.createElement("p");
+        para.textContent = card.para;
+        para.className = "text-slate-500 text-sm mt-1";
+
+        const meta = document.createElement("div");
+        meta.className = "mt-3 flex justify-between text-gray-700 text-sm";
+        const rateDiv = document.createElement('div');
+        rateDiv.className = 'flex gap-1'
+
+        let x = card.rating;
+
+        for (let i = 0; i <5; i++) {
+            const rate = document.createElement('span');
+            // rate.innerHTML = `<i class="fa-solid fa-star"></i>`
+            rate.innerHTML = `<span class="text-base material-symbols-outlined">star</span>`
+            rate.className = 'text-slate-400'
+
+            if (i < x) {
+                rate.className = "text-yellow-500"
+            }
+            rateDiv.appendChild(rate)
+        }
+
+        const level = document.createElement('span');
+        level.className = 'capitalize text-slate-500 content-center text-xs px-2 py-1 font-extralight bg-slate-200 rounded-md'
+        level.innerText = card.level
+        
+
+        meta.append(rateDiv, level);
+
+        contentDiv.append(heading, para, meta);
+        // contentDiv.appendChild(para);
+        // contentDiv.appendChild(meta);
+
+        cardDiv.appendChild(imgDiv);
+        imgDiv.appendChild(imgCon)
+        cardDiv.appendChild(contentDiv);
+
+        course.appendChild(cardDiv);
+    })
+}
+
+render();
